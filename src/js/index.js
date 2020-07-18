d363579b53f3142e50587eeb5acd3e762f37ffa9
WebersonRodrigues <weberson.r.a@gmail.com>
2020-07-18 11:05:53 -0300

    Resolver bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
+var valor = prompt('digite um numero');
+var numero  = parseInt(numero);
 
-document.write('<p>Testando a aplicação...</p>')
+if(isNaN(numero)){
+    alert('Você não digitou um numero - ' + valor);
+} else if(numero%2 != 0){
+    alert('Impar');
+}else{
+    alert('Par');
+}
+
+// Resolver esse problema e explicar 
+// aonde esta o bug e porque estava ocorrendo. ;)
